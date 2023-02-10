2ef14d69581531126f880dcac7a1425197082cbf
pasha_n <pasha.naruta@gmail.com>
2023-02-10 14:23:42 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'welcome/booking'
-  get 'welcome/confirmation'
+  get 'welcome/standard-booking'
+  get 'welcome/deluxe-booking'
+  get 'welcome/suite-booking'
+  get 'welcome/connected-booking'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
