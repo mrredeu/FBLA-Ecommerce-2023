e2d47ab84db0b29461660114245181d18691642b
pasha_n <pasha.naruta@gmail.com>
2023-02-19 09:54:53 -0700

    asdf

--- app/javascript/booking.js
+++ app/javascript/booking.js
@@ -1,28 +1,23 @@
+//Images slider
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlide(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function changeSlide(n) {
+  showSlide(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+function showSlide(n) {
+  const slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].className = slides[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].className += " active";
 }
+
+//calendar
