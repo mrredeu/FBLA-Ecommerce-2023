3dbcd757de538440d22dfa1228f0909734b7fd17
pasha_n <pasha.naruta@gmail.com>
2023-02-20 15:05:30 -0700

    sadf

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   resources :rooms
-  get 'welcome/index'
+  get 'welcome/main'
   get 'welcome/standardBooking'
   get 'welcome/deluxeBooking'
   get 'welcome/suiteBooking'
   get 'welcome/connectedBooking'
   get 'welcome/attractions'
   get 'welcome/aboutUs'
+  get 'rooms/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'welcome#index'
+  root 'welcome#main'
 end
