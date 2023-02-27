0328b621702b1a929dc2e7e6a1ab007fde90846f
pasha_n <pasha.naruta@gmail.com>
2023-02-26 17:31:28 -0700

    asdf

--- app/javascript/slider.js
+++ app/javascript/slider.js
@@ -1,21 +1,25 @@
 //Images slider
-let slideIndex = 1;
+var slideIndex = 1;
 showSlide(slideIndex);
 
 function changeSlide(n) {
-  showSlide(slideIndex += n);
+  showSlide((slideIndex += n));
 }
 
 function showSlide(n) {
-  const slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (let i = 0; i < slides.length; i++) {
     slides[i].className = slides[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  slides[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].className += " active";
 }
