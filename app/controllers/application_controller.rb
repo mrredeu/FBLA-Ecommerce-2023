db0428288f85ee2e68aa77b4ae90d6d5ff8d85d6
pasha_n <pasha.naruta@gmail.com>
2023-02-26 17:28:34 -0700

    asdf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base 
+end
+# app/controllers/data_controller.rb
+class DataController < ApplicationController
+    def index
+        @data = MyModel.all # replace MyModel with your model name
+        respond_to do |format|
+            format.json { render json: @data }
+        end
+    end
 end
