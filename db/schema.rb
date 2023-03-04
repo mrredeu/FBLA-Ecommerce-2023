24f12f79307fbd58c80f2e4c328d970bb2674743
pasha_n <pasha.naruta@gmail.com>
2023-03-03 20:10:54 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_002538) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_024446) do
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
